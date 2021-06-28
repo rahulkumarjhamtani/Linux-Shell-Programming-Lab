72e62dbf353faf551c6da7d92386091facae9fce
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-06-28 13:41:05 +0530

    array

--- reversearray.sh
+++ reversearray.sh
@@ -1,12 +1,15 @@
 echo "Number of elements in array : "
 read n
-sum=0
 echo "Elements in array : "
 i=0
 for ((i=0;i<n;i++))
 do
 read arr[$i]
-sum=$((sum+arr[$i]))
 done
-r=$((sum / n))
-echo "Average = "$r
+echo "Elements : "
+x=$((n-1))
+for (( i=$x;i>=0;i-- ))
+do
+r=$((arr[$i]))
+echo $r
+done
