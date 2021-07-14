32fa3df1a2b433a47012b79662b885a7d18fb975
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-07-14 14:09:24 +0530

    pattern

---
+++ pattern5.sh
@@ -0,0 +1,31 @@
+echo "Number of rows"
+read n
+for ((i=1;i<=n;i++))
+do
+    num=1
+    for ((j=i;j<=n;j++))
+    do
+        echo -n " "
+    done
+    for ((j=1;j<=2*i-1;j++))
+    do
+        echo -n $num
+        num=$((num+1))
+    done
+    echo
+done
+for ((i=n-1;i>=1;i--))
+do
+    num=1
+    for ((j=n;j>=i;j--))
+    do
+        echo -n " "
+    done
+    for ((j=1;j<=2*i-1;j++))
+    do
+        echo -n $num
+        num=$((num+1))
+    done
+    echo
+done
+
