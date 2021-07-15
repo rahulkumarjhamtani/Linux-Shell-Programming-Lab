da17f273b056d4a9ad2dc634b107146489c3975e
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-07-15 19:51:30 +0530

    lab

--- si.sh
+++ si.sh
@@ -1,13 +1,13 @@
-echo "Simple Interest => ";
+echo "Simple Interest => "
 
-echo "Principle = ";
-read p;
+echo "Principle = "
+read p
 
-echo "Rate = ";
-read r;
+echo "Rate = "
+read r
 
-echo "Time = ";
-read t;
+echo "Time = "
+read t
 
-si=`expr $p \* $r \* $t / 100`;
-echo "Simple Interest = "$si;
+si=`expr $p \* $r \* $t / 100`
+echo "Simple Interest = "$si
